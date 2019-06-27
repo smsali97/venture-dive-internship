1f20914a9a353b6f036948ef0b5f560c7cd6c720
Sualeh Ali <smsali97@gmail.com>
2019-06-27 18:29:13 +0500

    Started on database

---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/topic/TopicRepository.java
@@ -0,0 +1,15 @@
+package io.javabrains.springbootstarter.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+	// get me all the topics
+		
+	// get topic given id
+	
+	// update topic given topic
+	
+	// delete topic given id
+	
+	
+}
---
+++ course-api-data/src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,54 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+// Business Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+	
+	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
+			new Topic("spring","Spring Framwork","Spring Framework Description"),
+			new Topic("java","Core Java","Core Java Description"),
+			new Topic("javascript","JavaScript","JavaScript Description")
+			));
+	
+	
+	public List<Topic> getAllTopics() {
+		
+		
+		List<Topic> topics = new ArrayList<Topic>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(e -> e.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topicRepository.save(topic);
+	}
+	
+	public void deleteTopic(String id) {
+		topics.removeIf( e -> e.getId().equals(id) );
+	}
+	
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++ ) {
+			if (topics.get(i).getId().equals(id) ) { 
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+	
+}
